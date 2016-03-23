e3b2363e56979d1f3006f229d77bcdde2680eaa2
medubin <medubin@gmail.com>
2016-03-22 19:39:08 -0700

    quicksort

---
+++ Ruby/sorts/quick_sort.rb
@@ -0,0 +1,17 @@
+class Array
+  def quick_sort!(&prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+    return self if self.length <= 1
+    pivot = self[0]
+    left, right = [], []
+
+    self[1..-1].each do |el|
+      if prc.call(el, pivot) == -1
+        left << el
+      else
+        right << el
+      end
+    end
+    left.quick_sort!(&prc) + [pivot] + right.quick_sort!(&prc)
+  end
+end
